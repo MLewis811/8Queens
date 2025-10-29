5b123a76100c97dbefd082c0750f5f06c01887e4
Mike Lewis <revmikelewis@gmail.com>
2025-10-29 08:50:54 -0500

    Initial Commit

---
+++ 8Queens/_QueensApp.swift
@@ -0,0 +1,17 @@
+//
+//  _QueensApp.swift
+//  8Queens
+//
+//  Created by Mike Lewis on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct _QueensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
